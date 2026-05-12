e0406aef35f4c3120e785c476bbdc712f93e006f
lellermaa <lellermaa@gmail.com>
2026-05-12 18:20:29 +0300

    feat: HW-14 User Management API

---
+++ src/DTO/UserDTO.ts
@@ -0,0 +1,18 @@
+export class UserDTO {
+    id: number;
+    name: string;
+    email: string;
+    phone: string;
+
+    constructor(
+        id: number,
+        name: string,
+        email: string,
+        phone: string
+    ) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+}
